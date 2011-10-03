cfd3b4f7f169684d6874ca4cadc3b4d1db5aa779
Paulo Roberto Ribeiro <paulo@duocriativa.com.br>
2011-10-03 17:31:47 -0300

    Setting up the first version

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Duo\AtlassianCrowdAuthorizationBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
